670b2c0be7dc40376df3a5779879082497bd4c5c
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-02-12 09:17:59 +0600

    thanmks AIA, dark, SA

--- offline9_dynamic/visiting_cities.cpp
+++ offline9_dynamic/visiting_cities.cpp
@@ -15,23 +15,24 @@ long solve(int, int);
 
 int main()
 {
-    
+
     std::cin >> total;
 
-    for (int i = 0; i < MAXCITIES; i++)
-        for (int j = 0; j < nm; j++)
-            dp[i][j] = -1;
+    std::fill(dp[0], dp[0] + MAXCITIES * nm, -1);
 
     for (int row = 0; row < total; row++)
         for (int col = 0; col < total; col++)
             std::cin >> costs[row][col];
 
+    // we start with first city with no city visited
     std::cout << solve(0, (1 << total) - 1);
     return 0;
 }
 
 long solve(int curr, int mask)
 {
+
+    // base case: if all city visited, then no need to travel so cost for this cell 0
     if (curr >= total)
         return 0;
 
@@ -40,19 +41,28 @@ long solve(int curr, int mask)
 
     long curr_city_cost = inf;
 
+    // value for current city with this mask isn't counted yet, we do that here
+    // looping for all cities and counting if it is univisited in mask
     for (int city = 0; city < total; city++)
     {
-        
-        if ((mask & (1 << city)) != 0)
+        // unvisited city
+        if (mask & (1 << city))
         {
             long prev_cities_cost = 0;
-           
+
             for (int prev_city = 0; prev_city < total; prev_city++)
             {
-                if ((mask & (1 << prev_city)) == 0)
+                // pre visited cities add cost
+                if (!(mask & (1 << prev_city)))
                     prev_cities_cost += costs[city][prev_city];
             }
-            
+
+            // min of (cost for visiting this city from this mask
+            // , min from costs of visiting all previous allowable cities from this mask)
+            // thus we keep taking the minimum cost, at lowest mask, all city will be visited with min cost
+            // to find cost for visiting all the ciites in the mask without current city
+            // recursive call is made with current city's bit set in the mask and increasing the number of cities visited
+
             curr_city_cost = std::min(curr_city_cost,
                                       costs[city][city] + prev_cities_cost + solve(curr + 1, (mask ^ (1 << city))));
         }
